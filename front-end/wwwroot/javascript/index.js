39e250e70b9247570101320139f0899ec8cdacba
Sophie Shufan Wang <sophwa06@uw.edu>
2025-10-18 23:26:58 -0700

    added support for uploading pictures to mongodb as base64

--- front-end/wwwroot/javascript/index.js
+++ front-end/wwwroot/javascript/index.js
@@ -1,49 +1,53 @@
-function signUpFunction() {
-    app.get('/', async (req, res) => {
-        res.redirect('/input');
-    })
-}
+window.addEventListener("DOMContentLoaded", (event) => {
+    const form = document.querySelector('form');
+    const submitButton = document.getElementById('submit-input');
+    const fileInput = document.getElementById('file-input');
 
-function logInFunction() {
-    app.get('/', async (req, res) => {
-        res.redirect('/input');
-    })
-};
+    var data = new FormData(form)
 
-function uploadFiles() {
-    const input = document.getElementById('fileInput');
-    const files = input.files;
-
-    if (!files.length) {
-        displayMessage('No files selected.', 'red');
-        return;
-    }
-
-    const formData = new FormData();
-    for (let file of files) {
-        formData.append('upload[]', file);
-    }
-
-    const myDB = client.db("AI_Entry");
-    const myColl = myDB.collection("Images");
-
-    fetch('upload.php', {
-        method: 'POST',
-        body: formData,
-    })
-        .then(res => res.json())
-        .then(data => {
-            if (data.status === 'success') {
-                displayMessage('Upload successful!', 'green');
-            } else {
-                displayMessage(data.error || 'Upload failed.', 'red');
+    const getImageFile = (inputElement) => {
+        return new Promise(resolve => {
+            const reader = new FileReader()
+            reader.onload = function () {
+                resolve(reader.result)
             }
+            reader.readAsDataURL(inputElement);
         })
-        .catch(() => displayMessage('Upload error occurred.', 'red'));
-}
+    }
 
-function displayMessage(msg, color) {
-    const box = document.getElementById('message');
-    box.innerText = msg;
-    box.style.color = color;
-}
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        fetch('/file_upload', {
+            method: 'POST',
+            body: data
+        })
+            .then(data => console.log('Upload successful:', data))
+            .catch(error => console.error('Error uploading image:', error));
+    });
+    fileInput.addEventListener('change', async () => {
+        const imageFile = await getImageFile(fileInput["files"][0])
+        data.append("contents", imageFile);
+    });
+});
+
+
+
+// mongoose.connect('mongodb+srv://sophwa06_db_user:jY2QUhNLCVE8XrR3@dubhacks-test.wqvw1ns.mongodb.net/?retryWrites=true&w=majority&appName=DubHacks-Test');
+// const { Schema } = mongoose;
+
+// async function uploadFiles() {
+//     console.log('test')
+//     const imgData = new FormData(fileInput)
+//     const imgModel = mongoose.model('Image', { image: Mixed });
+//     const image = new imgModel({ image: imgData });
+
+
+//     try {
+//         result = await image.save();
+//         console.log(result);
+//         mongoose.disconnect();
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
--- front-end/wwwroot/routes/input.js
+++ front-end/wwwroot/routes/input.js
@@ -1,43 +1,65 @@
 import express from 'express';
 import fs from 'fs';
+import mongoose from 'mongoose';
+import multer from 'multer';
+
 var router = express.Router();
+let upload = multer({
+     limits: { fieldSize: 25 * 1024 * 1024 }
+});
+
+const { Schema } = mongoose;
+
+const imageSchema = new Schema({
+    image: Buffer
+})
+const Image = mongoose.model('Image', imageSchema)
 
 router.get('/', async (req, res) => {
     res.redirect('/site');
 })
 
-router.get('/site', async(req, res) =>{
+router.get('/site', async (req, res) => {
     res.type('html');
-    res.sendFile('index.html', {root: 'wwwroot'});
+    res.sendFile('index.html', { root: 'front-end/wwwroot' });
 })
 
-router.get('/site/*splat', async(req, res) =>{
+router.get('/site/*splat', async (req, res) => {
     let fileType = req.url.substring(req.url.lastIndexOf("."));
     res.type(fileType);
     let fileURL = req.url.replace("/site", "");
-    res.sendFile(fileURL, {root: 'wwwroot'});
+    res.sendFile(fileURL, { root: 'front-end/wwwroot' });
 })
 
-router.get('/input', async(req, res) =>{
+router.get('/input', async (req, res) => {
     res.type('html');
-    res.sendFile('input.html', {root: 'wwwroot'});
+    res.sendFile('input.html', { root: 'front-end/wwwroot' });
 });
 
-router.get('/sign-in', async(req, res) =>{
+router.get('/sign-in', async (req, res) => {
     res.type('html');
-    res.sendFile('sign-in.html', {root: 'wwwroot'});
+    res.sendFile('sign-in.html', { root: 'front-end/wwwroot' });
 });
 
-router.get('/file_upload', async(req, res) =>{
-    console.log(req.params);
-    console.log(req.body)
-    
-    res.send("success")
+router.post('/file_upload', upload.single('uploaded_file'), async (req, res) => {
+    mongoose.connect('mongodb+srv://sophwa06_db_user:jY2QUhNLCVE8XrR3@dubhacks-test.wqvw1ns.mongodb.net/?retryWrites=true&w=majority&appName=DubHacks-Test');
+    console.log("trying")
+    console.log(mongoose.connection.readyState);
+    console.log(req.file);
+    const imageUpload = new Image({ image: req.body.contents })
+
+    try {
+        await imageUpload.save()
+        mongoose.disconnect
+        res.redirect('/result')
+    } catch (error) {
+        console.log(error)
+    }
 });
 
-router.get('/result', async(req, res) =>{
+router.get('/result', async (req, res) => {
     res.type('html');
-    res.sendFile('result.html', {root: 'wwwroot'});
+    res.sendFile('result.html', { root: 'front-end/wwwroot' });
 });
 
 export default router
